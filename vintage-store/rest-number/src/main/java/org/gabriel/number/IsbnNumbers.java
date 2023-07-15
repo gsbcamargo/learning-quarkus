5252ab2e19c89d6f1776ec75467baa2338ff2291
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-07-14 23:42:14 -0300

    creates isbnNumbers generator

---
+++ vintage-store/rest-number/src/main/java/org/gabriel/number/IsbnNumbers.java
@@ -0,0 +1,11 @@
+package org.gabriel.number;
+
+import java.time.Instant;
+
+public class IsbnNumbers {
+
+    public String isbn10;
+    public String isbn13;
+    public Instant generationDate;
+    
+}
--- vintage-store/rest-number/src/main/java/org/gabriel/number/NumberResource.java
+++ vintage-store/rest-number/src/main/java/org/gabriel/number/NumberResource.java
@@ -1,5 +1,10 @@
 package org.gabriel.number;
 
+import java.time.Instant;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +13,18 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/api/numbers")
 public class NumberResource {
 
+    @Inject
+    Logger logger;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public IsbnNumbers generateIsbnNumbers() {
+        IsbnNumbers isbnNumbers = new IsbnNumbers();
+        isbnNumbers.isbn13 = "13-" + new Random().nextInt(100_000_000);
+        isbnNumbers.isbn10 = "10-" + new Random().nextInt(100_000);
+        isbnNumbers.generationDate = Instant.now();
+        logger.info("Numbers generated: " + isbnNumbers);
+
+        return isbnNumbers;
     }
 }
