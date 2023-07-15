f02be693bc673f518bf3b0770d8e2eb4fe210fb6
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-07-15 00:23:44 -0300

    adjusts jackson dependency and logger injection

--- vintage-store/rest-number/src/main/java/org/gabriel/number/IsbnNumbers.java
+++ vintage-store/rest-number/src/main/java/org/gabriel/number/IsbnNumbers.java
@@ -8,12 +8,36 @@ public class IsbnNumbers {
     public String isbn13;
     public Instant generationDate;
 
+    public String getIsbn10() {
+        return isbn10;
+    }
+
+    public void setIsbn10(String isbn10) {
+        this.isbn10 = isbn10;
+    }
+
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    public void setIsbn13(String isbn13) {
+        this.isbn13 = isbn13;
+    }
+
+    public Instant getGenerationDate() {
+        return generationDate;
+    }
+
+    public void setGenerationDate(Instant generationDate) {
+        this.generationDate = generationDate;
+    }
+
     @Override
     public String toString() {
         return "IsbnNumbers{" +
-            "isbn13='" + isbn13 + '\'' +
-            ", isbn10='" + isbn10 + '\'' +
-            ", generationDate=" + generationDate +
-            '}';
+                "isbn13='" + isbn13 + '\'' +
+                ", isbn10='" + isbn10 + '\'' +
+                ", generationDate=" + generationDate +
+                '}';
     }
 }
